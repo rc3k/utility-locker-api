87a0fb0b000e7027cea4128346ade1dc345ce85a
Rob Pickering <rwpickering@gmail.com>
2020-06-04 16:03:02 +0100

    A couple of minor amends to the products API

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,13 @@
 from rest_framework import routers
 
-from .views import ProductViewSet
+from django.conf.urls import url
+
+from .views import ProductViewSet, get_product_types
 
 app_name = 'products'
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet, basename='products')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    url(r'^producttypes/$', get_product_types, name='get_product_types'),
+]
--- products/views.py
+++ products/views.py
@@ -1,18 +1,19 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-from .models import Product
+from .models import Product, PRODUCT_TYPES
 from .serializers import ProductSerializer
 
 
 class ProductViewSet(ViewSet):
     """
-    a simple for viewset for returning a list of products with optional ordering and filtering by type
+    a simple viewset for retrieving a list of products with optional ordering and filtering by type
     """
 
     def list(self, request):
-        order_column = request.query_params.get('col', 'name').lower()
-        order_direction = request.query_params.get('dir', 'asc').lower()
+        order_column = request.query_params.get('column', 'name').lower()
+        order_direction = request.query_params.get('direction', 'asc').lower()
 
         queryset = Product.objects.all().order_by(
             '{}{}'.format(
@@ -27,3 +28,14 @@ class ProductViewSet(ViewSet):
 
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_product_types(request):
+    """
+    a simple viewset for retrieving a list of product types
+    """
+    return Response([{
+        'key': key,
+        'value': value,
+    } for key, value in sorted(PRODUCT_TYPES, key=lambda pt: pt[1])])
